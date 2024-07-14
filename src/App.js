e1c426ce553933eec91d1a76aa3c9fb4920f3a87
Palak Mishra <mishrapalak1804@gmail.com>
2024-07-14 20:58:32 +0530

    git commit README.md

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <h1 className='text-green-400'>hello</h1>
+      <h1 className='text-green-400 text-9xl font-extrabold'>hello</h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+// tailwind.config.js
+
+export const content = [
+  "./src/**/*.{js,jsx,ts,tsx}",
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
